ea02d8a975bb1507daf1abef31992f342427fd4e
Jacob Solomon <jmsolomon64@gmail.com>
2022-06-23 19:57:12 -0400

    Add project files.

---
+++ DungeonsAndDragons.Data/Entity/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons.Data.Entity
+{
+    public class Character
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int RaceId { get; set;}
+        [Required]
+        public int ClassId { get; set; }
+        [Required]
+        public int Strength { get; set; }
+        [Required]
+        public int Dexterity { get; set; }
+        [Required]
+        public int Consitution { get; set; }
+        [Required]
+        public int Inteligence { get; set; }
+        [Required]
+        public int Wisdom { get; set; }
+        [Required]
+        public int Charisma { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ DungeonsAndDragons.Data/Entity/Race.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons.Data.Entity
+{
+    public class Race
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(10000)]
+        public string Description { get; set; }
+    }
+}
